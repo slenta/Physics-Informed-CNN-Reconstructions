d4d74f3b40f5bcdbc2aebdd019d78f20437e835b
slenta <simon.lentz@yahoo.de>
2021-10-05 23:50:12 +0200

    Plot funktioniert

--- dataproccessing.py
+++ dataproccessing.py
@@ -1,15 +1,30 @@
 
+from os import name
 import netCDF4 as nc
 import numpy as np
 import pylab as plt
 
+#name the filepath
 fn = 'masken/tos_r8_mask_en4_1970.nc'
+name = 'Asmaske_1970'
 ds = nc.Dataset(fn)
 
+#extract the variables from the file
 lon = ds['lon'][:]
 lat = ds['lat'][:]
 time = ds['time'][:]
+sst = ds['tos'][:]
 
+#complete sst data to create symmetric shape
+rest = np.ones((754, 36, 256)) * 9999
+sst_new = np.concatenate((sst, rest), axis=1)
 
+#plot ssts in 2d plot
+pixel_plot = plt.figure()
+pixel_plot = plt.imshow(sst_new[1])
+plt.colorbar(pixel_plot)
+plt.savefig('masken/' + name + '.pdf')
+plt.show()
 
-print(lon[0:20], lat[0:20], time[0:20])
+print(lon[0][0])
+print(sst_new.shape)
