8636861e4a97ec3fccbc3de817cab1bd9a767e72
slenta <simon.lentz@yahoo.de>
2021-10-05 16:17:15 +0200

    metadaten angucken

--- dataproccessing.py
+++ dataproccessing.py
@@ -6,5 +6,9 @@ import pylab as plt
 fn = 'masken/tos_r8_mask_en4_1970.nc'
 ds = nc.Dataset(fn)
 
+lon = ds['lon'][:]
+lat = ds['lat'][:]
+time = ds['time'][:]
 
-print(ds)
+
+print(ds, lon[0:20], lat[0:20], time[0:20])
