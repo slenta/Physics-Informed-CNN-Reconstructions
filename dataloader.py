b244a679876d36ee80178e9bc47bef9f569ca717
slenta <simon.lentz@yahoo.de>
2021-10-25 01:10:06 +0200

    dataloader more changes

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,9 @@
 import math
-
+import pylab as plt
 import numpy as np
 import netCDF4 as nc
 import h5py
+import torch
 from torch.utils.data import Dataset
 
 
@@ -10,15 +11,19 @@ from torch.utils.data import Dataset
 
 #dataloader and dataloader
 
-def preprocessing(path, name, plot):
+def preprocessing(path, name, type, plot):
     
-    ds = nc.Dataset(path)
+    ds = nc.Dataset(path + name + '.nc')
         
     #extract the variables from the file
     lon = ds['lon'][:]
     lat = ds['lat'][:]
     time = ds['time'][:]
-    sst = ds['tos'][:]
+
+    if type == 'mask':
+        sst = ds['sao'][:]
+    elif type == 'image':
+        sst = ds['tos'][:]
 
 
     #complete sst data to create symmetric shape
@@ -26,38 +31,39 @@ def preprocessing(path, name, plot):
     sst_new = np.concatenate((sst, rest), axis=1)
 
     #plot ssts in 2d plot
-    if plot == true:
+    if plot == True:
         pixel_plot = plt.figure()
         pixel_plot = plt.imshow(sst_new[1], vmin=-30, vmax=45)
         plt.colorbar(pixel_plot)
-        plt.savefig('Asi_maskiert/images/' + name + '.pdf')
+        plt.savefig('Asi_maskiert/pdfs/' + name + '.pdf')
 
     #create new h5 file with symmetric ssts
-    f = h5py.File(name + '.hdf5', 'w')
+    f = h5py.File(path + name + '.hdf5', 'w')
     sst_new = f.create_dataset('tos_sym', (754, 256, 256))
     f.close()
 
 
 class MaskDataset(Dataset):
 
-    def __init__(self, year)):
+    def __init__(self, year):
         super(MaskDataset, self).__init__()
 
-        self.image_path = 'Asi_maskiert/original_images/tos_r8_mask_en4_' + year
-        self.mask_path = 'Asi_maskiert/original_masks/tos_r8_mask_en4_' + year
-        self.masked_images_path = 'Asi_maskiert/masked_images/tos_r8_mask_en4_' + year
+        self.image_path = 'Asi_maskiert/original_image/'
+        self.mask_path = 'Asi_maskiert/original_masks/'
+        self.masked_images_path = 'Asi_maskiert/masked_images/'
+        self.year = year
 
-    def __getitem__(self, year, index):
+    def __getitem__(self, index):
 
         #create h5 datasets
-        preprocessing(self.image_path + 'nc', 'As_maske_' + year, plot = True)
-        preprocessing(self.mask_path + year + 'nc', 'As_maske_' + year, plot = False)
-        preprocessing(self.masked_images_path + year + 'nc', 'As_maske_' + year, plot = True)
+        preprocessing(self.image_path , 'Assimilation_1958_2020', 'image', True)
+        preprocessing(self.mask_path, 'Maske_' + self.year, 'mask', False)
+        preprocessing(self.masked_images_path, 'tos_r8_mask_en4_' + self.year, 'image', True)
 
         #get h5 file for image, mask, image plus mask and define relevant variables (tos)
-        f_image = h5py.File(self.image_path + year '.hdf5')
-        f_mask = h5py.File(self.mask_path + year '.hdf5')
-        f_masked_image = h5py.File(self.masked_images_path + year '.hdf5')
+        f_image = h5py.File(self.image_path + '.hdf5')
+        f_mask = h5py.File(self.mask_path + self.year + '.hdf5')
+        f_masked_image = h5py.File(self.masked_images_path + self.year + '.hdf5')
 
         #extract sst data/mask data
         image_data = f_image.get('tos_sym')
@@ -74,13 +80,13 @@ class MaskDataset(Dataset):
     def __len__(self):
         
         #print length of time dimension
-        f_image = h5py.File(self.image_path + year '.hdf5')
+        f_image = h5py.File(self.image_path + self.year + '.hdf5')
         image_data = f_image.get('tos_sym')
         n_samples = len(image_data[:, 1, 1])
         return n_samples
 
 #create dataset
-dataset = MaskDataset()
+dataset = MaskDataset('2020')
 
 #get sample and unpack
 first_data = dataset[0]
