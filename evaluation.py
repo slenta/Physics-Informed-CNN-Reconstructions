58615a601180dc3713a31ed11a3ef9e8c9b36fb0
slenta <simon.lentz@yahoo.de>
2022-01-06 14:35:28 +0100

    eval error

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import shape
 import torch
 from torch.utils import data
 from torchvision.utils import make_grid
@@ -20,7 +21,7 @@ def evaluate(model, dataset, device, filename):
     with torch.no_grad():
         output, _ = model(image.to(device), mask.to(device))
     output = output.to(torch.device('cpu'))
-    print(output.shape, np.shape(np.array(mask)), np.shape(np.array(image)), np.shape(np.array(gt)))
+    #print(output.shape, np.shape(np.array(mask)), np.shape(np.array(image)), np.shape(np.array(gt)))
     output_comp = mask*image + (1 - mask)*output
     #print(output_comp.shape)
     grid = make_grid(
@@ -28,12 +29,14 @@ def evaluate(model, dataset, device, filename):
                    unnormalize(output_comp), unnormalize(gt)), dim=0))
 
     n = image.shape
-    print(n)
+    error_map = np.sqrt(image - output)**2
+
     f = h5py.File(filename + '.hdf5', 'w')
     dset1 = f.create_dataset('image', (n[1], n[2], n[3]), dtype = 'float32',data = image)
     dset2 = f.create_dataset('output', (n[1], n[2], n[3]), dtype = 'float32',data = output)
     dset3 = f.create_dataset('output_comp', (n[1], n[2], n[3]), dtype = 'float32',data = output_comp)
     dset4 = f.create_dataset('mask', shape=(n[1], n[2], n[3]), dtype='float32', data=mask)
+    dset5 = f.create_dataset('errormap', shape(n[1], n[2], n[3]), dtype='float32', data=error_map)
     f.close()
     
     save_image(grid, filename + '.jpg')
