c9dac21b77993eaf1bac43b69528011013f35909
slenta <simon.lentz@yahoo.de>
2022-01-06 17:29:54 +0100

    dataloader split

--- dataloader.py
+++ dataloader.py
@@ -78,62 +78,66 @@ def preprocessing(path, name, year, type, plot):
 
 class MaskDataset(Dataset):
 
-    def __init__(self, year):
+    def __init__(self, year, mode):
         super(MaskDataset, self).__init__()
 
         self.image_path = '../Asi_maskiert/original_image/'
         self.mask_path = '../Asi_maskiert/original_masks/'
-        self.masked_images_path = '../Asi_maskiert/masked_images/'
         self.image_name = 'Image_'
         self.mask_name = 'Maske_'
         self.image_year = '2020'
         self.masked_images_name = 'tos_r8_mask_en4_'
         self.year = year
+        self.mode = mode
 
     def __getitem__(self, index):
 
         #get h5 file for image, mask, image plus mask and define relevant variables (tos)
         f_image = h5py.File(self.image_path + self.image_name + self.image_year + '.hdf5', 'r')
         f_mask = h5py.File(self.mask_path + self.mask_name + self.year + '.hdf5', 'r')
-        #f_masked_image = h5py.File(self.masked_images_path + self.masked_images_name + self.year + '.hdf5', 'r')
 
         #extract sst data/mask data
-        image_data = f_image.get('tos_sym')
-        #print(image_data)
+        image = f_image.get('tos_sym')
         mask_data = f_mask.get('tos_sym')
-        #masked_image_data = f_masked_image.get('tos_sym')
+
+        n = image.shape
+        im_new = []
+
+        if self.mode == 'train':
+            for i in range(n[0]):
+                if i%5 >= 1:
+                    im_new.append(image[i])
+        elif self.mode == 'val':
+            for i in range(n[0]):
+                if i%5 == 0:
+                    im_new.append(image[i])
+
+        im_new = np.array(im_new)
 
         #convert to pytorch tensors
-        image = torch.from_numpy(image_data[index, :, :])
+        im_new = torch.from_numpy(im_new[index, :, :])
         mask = torch.from_numpy(mask_data[index, :, :])
-        #masked_image = torch.from_numpy(masked_image_data[index, :, :])
 
-        #image = torch.unsqueeze(image, dim=0)
-        #mask = torch.unsqueeze(mask, dim=0)
-        #masked_image = torch.unsqueeze(masked_image, dim=0)
-
+        #Repeat to fit input channels
         mask = mask.repeat(3, 1, 1)
-        image = image.repeat(3, 1, 1)
-        #masked_image = masked_image.repeat(3, 1, 1)
-        #print(mask.shape)
+        im_new = im_new.repeat(3, 1, 1)
 
-        return mask*image, mask, image
+        return mask*im_new, mask, im_new
 
     def __len__(self):
         
-        #print length of time dimension
-        f_image = h5py.File(self.image_path + self.image_name + self.year +  '.hdf5', 'r')
-        image_data = f_image.get('tos_sym')
-        n_samples = len(image_data[:, 1, 1])
-        return n_samples
+        mi, ma, im_new = self.__getitem__(0)
+        n = im_new.shape
+        length = n[0]
+
+        return length
+
+
 
 #create dataset
-dataset = MaskDataset('2020')
+#dataset = MaskDataset('2020')
 
 #get sample and unpack
-first_data = dataset
-masked_image, mask, image = first_data[0]
-
-#print(torch.any(image.isnan()))
-
+#first_data = dataset
+#masked_image, mask, image = first_data[0]
 
