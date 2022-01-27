611be6a59b27e47d08caccaaa54d14546234d946
slenta <simon.lentz@yahoo.de>
2022-01-27 19:17:32 +0100

    preprocessing rm from dataloader

--- dataloader.py
+++ dataloader.py
@@ -9,64 +9,6 @@ import xarray as xr
 import config as cfg
 
 
-#dataloader and dataloader
-
-def preprocessing(path, name, year, type, plot):
-    
-    #ds = nc.Dataset(path + 'Asi_maskiert/masked_images/'name + '.nc')
-    ds = xr.load_dataset(path + name + year + '.nc', decode_times=False)
-
-    #extract the variables from the file
-    if type == 'mask':
-        sst = ds.tho.values
-        x = np.isnan(sst)
-        n = sst.shape
-        for i in range(n[0]):
-            for j in range(n[1]):
-                for k in range(n[2]):
-                    for l in range(n[3]):
-                        if np.isnan(sst[i, j, k, l]) == True:
-                            sst[i, j, k, l] = 0
-                        else:
-                            sst[i, j, k, l] = 1
-        
-        rest = np.zeros((n[0], n[1], n[3] - n[2], n[3]))
-        sst_new = np.concatenate((sst, rest), axis=2)
-
-        #create new h5 file with symmetric ssts
-        f = h5py.File(path + name + year + '.hdf5', 'w')
-        dset1 = f.create_dataset('tos_sym', (n[0], n[1], n[3], n[3]), dtype = 'float32', data = sst_new)
-        f.close()
-
-    if type == 'image':
-        sst = ds.thetao.values
-        x = np.isnan(sst)
-        n = sst.shape
-        sst[x] = 0
-        rest = np.zeros((n[0], n[1], n[3] - n[2], n[3]))
-        sst_new = np.concatenate((sst, rest), axis=2)
-
-        #create new h5 file with symmetric ssts
-        f = h5py.File(path + name + year + '.hdf5', 'w')
-        dset1 = f.create_dataset('tos_sym', (n[0], n[1], n[3], n[3]), dtype = 'float32',data = sst_new)
-        f.close()
-
-    #plot ssts in 2d plot
-    if plot == True:
-        pixel_plot = plt.figure()
-        pixel_plot = plt.imshow(sst_new[1], vmin = -5, vmax = 5)
-        plt.colorbar(pixel_plot)
-        plt.savefig('../Asi_maskiert/pdfs/' + name + '.pdf')
-        plt.show()
-        
-
-
-#preprocessing('../Asi_maskiert/original_masks/', 'Maske_', '1970', type='mask', plot = False)
-#preprocessing('../Asi_maskiert/original_image/', 'Image_', 'r10', type='image', plot=False)
-#preprocessing('../Asi_maskiert/Chris_Daten/', 'Chris_image', type='image', plot=True)
-#preprocessing('../Asi_maskiert/Chris_Daten/', 'Chris_masks', type='mask', plot=True)
-
-
 class MaskDataset(Dataset):
 
     def __init__(self, mask_year, im_year, mode):
