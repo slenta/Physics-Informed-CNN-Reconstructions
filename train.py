baa92aabcf36200e439afae5f9741821a2d654df
slenta <simon.lentz@yahoo.de>
2022-05-03 11:56:34 +0200

    matplotlib Agg

--- train.py
+++ train.py
@@ -53,8 +53,8 @@ dataset_train = MaskDataset(cfg.im_year, depth, cfg.in_channels, mode='train')
 dataset_test = MaskDataset(cfg.im_year, depth, cfg.in_channels, mode='test')
 
 iterator_train = iter(DataLoader(dataset_train, batch_size=cfg.batch_size,
-                                 sampler=InfiniteSampler(len(dataset_train)),
-                                 num_workers=cfg.n_threads), shuffle=True)
+                                 sampler=InfiniteSampler(len(dataset_train), shuffle=True),
+                                 num_workers=cfg.n_threads))
 
 # define network model
 lstm = True
