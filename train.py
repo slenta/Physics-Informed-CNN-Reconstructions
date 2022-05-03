93696275edb44795cd489a009c352b7a84b73d99
slenta <simon.lentz@yahoo.de>
2022-05-03 12:10:12 +0200

    matplotlib Agg

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ else:
 dataset_train = MaskDataset(cfg.im_year, depth, cfg.in_channels, mode='train')
 dataset_test = MaskDataset(cfg.im_year, depth, cfg.in_channels, mode='test')
 
-iterator_train = iter(DataLoader(dataset_train, batch_size=cfg.batch_size, shuffle=True,
+iterator_train = iter(DataLoader(dataset_train, batch_size=cfg.batch_size,
                                  sampler=InfiniteSampler(len(dataset_train)),
                                  num_workers=cfg.n_threads))
 
