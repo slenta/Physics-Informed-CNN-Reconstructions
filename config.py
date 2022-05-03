ec58a94af07cec46a7f141bd7326b50b4e21559e
slenta <simon.lentz@yahoo.de>
2022-05-03 12:30:06 +0200

    evaluation

--- config.py
+++ config.py
@@ -96,7 +96,7 @@ def set_train_args():
     arg_parser.add_argument('--finetune', action='store_true')
     arg_parser.add_argument('--lr', type=float, default=2e-4)
     arg_parser.add_argument('--lr-finetune', type=float, default=5e-5)
-    arg_parser.add_argument('--max_iter', type=int, default=400000)
+    arg_parser.add_argument('--max_iter', type=int, default=2)
     arg_parser.add_argument('--log-interval', type=int, default=None)
     arg_parser.add_argument('--save-snapshot-image', action='store_true')
     arg_parser.add_argument('--depth', action='store_true')
