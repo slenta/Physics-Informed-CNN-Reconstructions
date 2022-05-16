c8057581ac77c392271ef0311a52ff44c09bf8df
slenta <simon.lentz@yahoo.de>
2022-05-16 19:11:27 +0200

    preprocessing masks

--- train.py
+++ train.py
@@ -132,9 +132,9 @@ for i in tqdm(range(start_iter, cfg.max_iter)):
     if (i + 1) % cfg.val_interval == 0:
         model.eval()
         prepo = preprocessing(cfg.im_dir, cfg.im_name, cfg.eval_im_year, cfg.image_size, 'image', cfg.in_channels, cfg.attribute_depth, cfg.attribute_anomaly, cfg.attribute_argo, cfg.lon1, cfg.lon2, cfg.lat1, cfg.lat2)
-        prepo_obs = preprocessing(cfg.mask_dir, cfg.mask_name, cfg.eval_mask_year, cfg.image_size, 'val', cfg.in_channels, cfg.attribute_depth, cfg.attribute_anomaly, cfg.attribute_argo, cfg.lon1, cfg.lon2, cfg.lat1, cfg.lat2)
-        prepo_obs.save_data()
-        prepo.save_data()
+        #prepo_obs = preprocessing(cfg.mask_dir, cfg.mask_name, cfg.eval_mask_year, cfg.image_size, 'val', cfg.in_channels, cfg.attribute_depth, cfg.attribute_anomaly, cfg.attribute_argo, cfg.lon1, cfg.lon2, cfg.lat1, cfg.lat2)
+        #prepo_obs.save_data()
+        #prepo.save_data()
         depths = prepo.depths()
 
         val_dataset = MaskDataset(cfg.eval_im_year, depth, cfg.in_channels, 'eval', shuffle=False)
