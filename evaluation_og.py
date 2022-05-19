7609c760f96b0e4611e8db53bfcc7fb2b5451d04
slenta <simon.lentz@yahoo.de>
2022-05-19 13:40:57 +0200

    time

--- evaluation_og.py
+++ evaluation_og.py
@@ -164,6 +164,8 @@ def heat_content_timeseries_masked(depth_steps, im_year, mask_year):
     obs = fo.get('tos_sym')
 
     obs_binary = np.where(np.isnan(obs)==False, 1, obs)
+    
+    plt.figure()
     plt.imshow(obs_binary[0, 0, :, :])
     plt.savefig(cfg.save_dir + 'obs_binary.pdf')
     plt.show()
