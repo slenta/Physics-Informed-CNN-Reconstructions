d4b0b718553180fd1a6493bc83d9edee6ec6cefa
slenta <simon.lentz@yahoo.de>
2022-05-19 21:34:52 +0200

    evaluate

--- evaluation_og.py
+++ evaluation_og.py
@@ -163,7 +163,7 @@ def heat_content_timeseries_masked(depth_steps, im_year, mask_year):
     obs = fo.get('tos_sym')
     obs = np.array(obs)
 
-    obs_binary = np.where(obs==0, np.nan, obs)
+    obs = np.where(obs==0, np.nan, obs)
     obs_binary = np.where(np.isnan(obs_binary)==False, 1, obs_binary)
     print(obs_binary.shape)
     
@@ -204,6 +204,7 @@ def compare_datasets(obs_path, im_path, name):
 
 
     obs = np.where(obs==0, np.nan, obs)
+    plt.figure()
     obs_binary = np.where(np.isnan(obs_binary)==False, 1, obs_binary)
     print(obs_binary.shape)
     masked = obs_binary*image
