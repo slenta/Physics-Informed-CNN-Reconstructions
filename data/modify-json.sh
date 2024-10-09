b427cc3bb944bb07eb608585a4b8333682e43608
Simon Lentz <u301617@levante5.lvt.dkrz.de>
2024-10-10 00:14:35 +0200

    retry

---
+++ data/modify-json.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the directory containing the JSON files
+json_dir="./train/"
+
+# Define the old and new phrases
+old_phrase="/assimilation_nc_files/"
+new_phrase="/input-goratz/"
+
+# Find all JSON files in the directory and replace the phrase
+find "$json_dir" -name "*.json" | while read -r file; do
+  sed -i "s#$old_phrase#$new_phrase#g" "$file"
+  echo "Updated $file"
+done
+
+echo "All JSON files have been updated."
