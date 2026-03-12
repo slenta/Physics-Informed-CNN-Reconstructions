1417ada9e195dbef07a879ce9a19e8777560ed91
Goratz Beobide Arsuaga <u241308@levante1.lvt.dkrz.de>
2026-03-12 14:06:03 +0100

    adapting scripts

--- climatereconstructionAI/input/slurm-scripts/eval-cnn.sh
+++ climatereconstructionAI/input/slurm-scripts/eval-cnn.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=10:00:00
 #SBATCH --mem=480G
 #SBATCH --constraint a100_80
-#SBATCH --output=outputs/eval-cnn.o%j 
+#SBATCH --output=/work/uo1075/u241308/ML_downscaling/code/climatereconstructionAI/input/slurm-scripts/eval_slurm_output/eval-cnn.o%j 
 
 module load python3/2022.01-gcc-11.2.0
 cd /work/uo1075/u301617/Master_Arbeit/code-goratz/
--- climatereconstructionAI/input/slurm-scripts/train-cnn.sh
+++ climatereconstructionAI/input/slurm-scripts/train-cnn.sh
@@ -7,10 +7,11 @@
 #SBATCH --time=10:00:00
 #SBATCH --mem=480G
 #SBATCH --constraint a100_80
-#SBATCH --output=outputs/train-cnn.o%j 
+#SBATCH --output=/work/uo1075/u241308/ML_downscaling/code/climatereconstructionAI/input/slurm-scripts/train_slurm_output/train-cnn.o%j 
 
-module load python3/2023.01-gcc-11.2.0
+#module load python3/2023.01-gcc-11.2.0
+module load python3
+source activate crai_downscalling
 cd /work/uo1075/u241308/ML_downscaling/code/climatereconstructionAI/
 
-source activate crai_downscalling
-python -m climatereconstructionai.train --load-from-file ./input/levante/train-crai-downscalling.txt
+python -m climatereconstructionai.train --load-from-file ./input/levante/train-crai-downscalling.txt --max-iter 700000
